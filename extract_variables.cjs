e3afe6ad9a88b09517fc3f2e770e569590dd7f76
glosslabsnj-hub <glosslabsnj@gmail.com>
2026-02-17 22:43:21 -0500

    Clean up repository: remove temporary development scripts and agent config dumps
    Removed 87 temporary files including:
    - Development/testing .cjs scripts
    - Agent configuration JSON dumps
    - Temporary prompt .txt files
    - Old agent system prompts
    - Tool configuration results
    
    All files moved to dev-archive/ directory (gitignored) for local reference.

--- extract_variables.cjs
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-
-const config = JSON.parse(fs.readFileSync('./dispatch_agent_config.json', 'utf8'));
-const vars = config.conversation_config.agent.dynamic_variables.dynamic_variable_placeholders;
-
-console.log("Total dynamic variables:", Object.keys(vars).length);
-console.log("\nAll variables:");
-console.log(Object.keys(vars).sort().join('\n'));
